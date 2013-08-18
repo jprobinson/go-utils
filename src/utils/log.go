da9e2836e574bca807722adf26b1927f10b7a367
JP Robinson <john.paul.robinson@gmail.com>
2013-08-18 13:45:03 -0400

    cleanup for initial push

--- src/utils/log.go
+++ src/utils/log.go
@@ -1,46 +1,45 @@
-package log
+package utils
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "os/signal"
-    "syscall"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type LogSetup interface {
-    SetupLogging()
+	SetupLogging()
+}
+
+// ListenForLogSignal listens for a SIGHUP signal from the OS. When it recieves the signal,
+// it will call the l to setup logging again.
+func ListenForLogSignal(l LogSetup) {
+	logSignal := make(chan os.Signal, 1)
+	signal.Notify(logSignal, syscall.SIGHUP)
+	for {
+		<-logSignal
+		l.SetupLogging()
+	}
 }
 
 type DefaultLogSetup struct {
-    LogFile string
-}
-
-func GetLogFileHandle(log_file_name string) *os.File {
-    log_file, err := os.OpenFile(log_file_name, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
-    if err != nil {
-        panic(fmt.Sprintf("Unable to get a hold of log file: %s", log_file_name))
-    }
-    return log_file
+	LogFile string
 }
 
 // SetupLogging attempts to get a handle on the given log file then sets it to the log output.
 // This function will also setup the log flags to include the go file name and line number.
-func (log_setup DefaultLogSetup) SetupLogging() {
-    log_file := GetLogFileHandle(log_setup.LogFile)
-    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-    log.SetOutput(log_file)
-    log.Print("setup default logger")
+func (l DefaultLogSetup) SetupLogging() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(GetLogFileHandle(l.LogFile))
+	log.Print("setup default logger")
 }
 
-// ListenForLogSignal listens for a SIGHUP signal from the OS. When it recieves the signal,
-// it will call the LogConfig to setup logging again.
-func ListenForLogSignal(log_setup LogSetup) {
-    log_signal := make(chan os.Signal, 1)
-    signal.Notify(log_signal, syscall.SIGHUP)
-    for {
-        <-log_signal
-        log_setup.SetupLogging()
-    }
+func GetLogFileHandle(logFileName string) io.Writer {
+	logFile, err := os.OpenFile(logFileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to get a hold of log file: %s", logFile))
+	}
+	return logFile
 }
-
